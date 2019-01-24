667ebdfb27c8227505c04dddb167659c9281e8a7
apple <apple@appledeMacBook-Air.local>
2019-01-24 19:35:58 +0800

    发表文章功能实现

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,8 @@ const router = require('koa-router')()
 const checkNotLogin = require('./../middlewares/check').checkNotLogin
 const checkLogin = require('../middlewares/check').checkLogin
 const userModel = require('./../lib/mysql')
-const md = require('markdown-it')
+const md = require('markdown-it')()
+const moment = require('moment')
 
 router.get('/', async (ctx, next) => {
     ctx.redirect('/posts')
@@ -58,5 +59,52 @@ router.post('/posts/self/page', async(ctx, next) => {
         })
 })
 
+// 发表文章页面
+router.get('/create', async(ctx, next) => {
+    await ctx.render('create', {
+        session: ctx.session,
+    })
+})
+
+// post 发表文章
+router.post('/create', async(ctx, next) => {
+    let title = ctx.request.body.title,
+        content = ctx.request.body.content,
+        id = ctx.session.id,
+        name = ctx.session.user,
+        time = moment().format('YYYY-MM-DD HH:mm:ss'),
+        avator,
+        // 现在使用markdown不需要单独转义
+        newContent = content.replace(/[<">']/g, (target) => { 
+            return {
+                '<': '&lt;',
+                '"': '&quot;',
+                '>': '&gt;',
+                "'": '&#39;'
+            }[target]
+        }),
+        newTitle = title.replace(/[<">']/g, (target) => {
+            return {
+                '<': '&lt;',
+                '"': '&quot;',
+                '>': '&gt;',
+                "'": '&#39;'
+            }[target]
+        });
+
+    console.log([name, newTitle, content, id, time])
+    await userModel.findUserData(ctx.session.user)
+        .then(res => {
+            console.log(res[0]['avator'])
+            avator = res[0]['avator']       
+        })
+    await userModel.insertPost([name, newTitle, md.render(content), content, id, time,avator])
+            .then(() => {
+                ctx.body = true
+            }).catch(() => {
+                ctx.body = false
+            })
+
+})
 
 module.exports = router;
--- routes/signout.js
+++ routes/signout.js
@@ -1,7 +1,7 @@
 const router = require('koa-router')()
 
 router.get('/signout', async (ctx, next) => {
-    ctx.sessio  = null
+    ctx.session = null
     console.log('退出成功')
     ctx.body = true
 })
